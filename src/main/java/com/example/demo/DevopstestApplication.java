012760944494c2e2ca2ac9af1ca156e672a57c57
NeerajaKuna <112148471+NeerajaKuna@users.noreply.github.com>
2024-04-02 14:06:24 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/DevopstestApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopstestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopstestApplication.class, args);
+		System.out.println("Hello world!");
+	}
+
+}
